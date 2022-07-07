2d8cbc1665783eb2d02d7fc1ae59c80f8f8bc825
Katarzyna Skibowska <kat.skibowska@gmail.com>
2022-07-07 20:44:21 +0200

    Strings added

--- test.js
+++ test.js
@@ -1,10 +1,4 @@
-var a = "8";
-var b = 5;
+var string1 = "Abc";
+var string2 = "Bcd";
 
-//double negative
-if (!isNaN(a)) {
-
-    console.log("meaning of life is " + (a * b));
-  
-}
-
+console.log(string1 < string2)
